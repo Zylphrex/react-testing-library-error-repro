de2e061e2650d3d296326fd774328dda179bc427
eps1lon <silbermann.sebastian@gmail.com>
2022-04-13 15:22:48 +0200

    repro without RTL

--- test.js
+++ test.js
@@ -1,15 +1,34 @@
-import React, {useEffect} from "react";
-import { render } from "@testing-library/react";
+import React, { useEffect } from "react";
+import * as ReactDOM from "react-dom";
+import * as ReactDOMTestUtils from "react-dom/test-utils";
 
 function Component() {
   useEffect(() => () => {}, []); // make sure to return a cleanup
   return <>x</>;
 }
 
-it("it", function () {
-  const {findByText} = render(<Component />);
-  return findByText("x").then(() => {
-  // the test should have timed out at this point
-    return findByText("x");
+let baseElement = null;
+let container = null;
+beforeEach(() => {
+  baseElement = document.body;
+  container = baseElement.appendChild(document.createElement("div"));
+});
+afterEach(() => {
+  ReactDOMTestUtils.act(() => {
+    ReactDOM.unmountComponentAtNode(container);
   });
-}, 1); // force a timeout of 1ms so that by the time the second `findByText` is called, the test should've failed
+  baseElement.removeChild(container);
+  container = null;
+});
+
+it("it", function () {
+  ReactDOMTestUtils.act(() => {
+    ReactDOM.render(<Component />, container);
+  });
+
+  return Promise.resolve().then(() => {
+    return ReactDOMTestUtils.act(() => {
+      return Promise.resolve();
+    }).then(() => {});
+  });
+}, 1); // force a timeout
